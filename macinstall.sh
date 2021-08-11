1edaf5dec925e41f7450ea06eb9bf76901eb0890
roklseky <62904571+roklseky@users.noreply.github.com>
2021-08-11 16:36:04 +0200

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -5,3 +5,31 @@
 brew tap caskroom/cask
 brew install caskroom/cask/brew-cask
 
+brew install wget
+brew install telnet
+
+brew install docker
+brew install --cask visual-studio-code
+brew install --cask pycharm-ce
+brew install --cask kodi
+brew install termius
+brew install --cask deepl
+brew install --cask trezor-suite
+brew install --cask tunnelblick
+brew install --cask tuxera-ntfs
+brew install --cask vlc
+brew install --cask raspberry-pi-imager
+brew install --cask itsycal
+brew install --cask teamviewer
+brew install --cask firefox
+brew install --cask airflow
+brew install --cask enpass
+brew install --cask teamviewer
+git clone https://github.com/marcelstoer/nodemcu-pyflasher.git
+
+wget https://www.python.org/ftp/python/3.9.6/python-3.9.6-macosx10.9.pkg
+wget https://downloads.arduino.cc/arduino-1.8.15-macosx.zip
+wget https://dl2.cdn.filezilla-project.org/client/FileZilla_3.55.1_macosx-x86.app.tar.bz2?h=Yl5oCqnblpnu2AlICfgTOA&x=1628694714
+
+
+
