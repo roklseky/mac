5e71d9f7609fdacb0169f212c4662f938f45f13a
roklseky <62904571+roklseky@users.noreply.github.com>
2021-08-11 16:38:56 +0200

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #https://formulae.brew.sh/cask/
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/cask
 brew install caskroom/cask/brew-cask
