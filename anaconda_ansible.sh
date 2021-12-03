40ecc2164738b1ded4b5ba9b39c33c3a43330619
roklseky <62904571+roklseky@users.noreply.github.com>
2021-12-03 19:40:53 +0100

    Create anaconda_ansible.sh

---
+++ anaconda_ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://opensource.com/article/19/8/using-conda-ansible-administration-macos
+
+conda create --name ansible-env --clone base
+conda activate ansible-env
+conda env list
+
+conda install -c conda-forge ansible
